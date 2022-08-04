f3fb800ab1ac39da724e73654f2616c640891ab6
Mark Harrison <mark@harrison.ws>
2022-08-04 21:20:47 +0100

    Upgrade .NET 6 and convert Microsoft.Azure.Storage.Blob to Azure.Storage.Blobs

--- appconfig.cs
+++ appconfig.cs
@@ -26,13 +26,17 @@ namespace ThingsReportIt
             _LogicAppEndpoint = config.GetValue<string>("LogicAppEndpoint");
             if (_LogicAppEndpoint == null || _LogicAppEndpoint == "")
             {
-                Debug.WriteLine("LogicAppEndpoint is not configured");
+                var msg = "LogicAppEndpoint is not configured";
+                Console.WriteLine(msg);
+                throw new Exception(msg);
             }
 
             _ThingsStorageConnectionString = config.GetConnectionString("ThingsStorageConnectionString");
             if (_ThingsStorageConnectionString == null || _ThingsStorageConnectionString == "")
             {
-                Debug.WriteLine("ThingsStorageConnectionString is not configured");
+                var msg = "ThingsStorageConnectionString is not configured";
+                Console.WriteLine(msg);
+                throw new Exception(msg);
             }
 
             _ImagesContainer = config.GetValue<string>("ImagesContainer");
@@ -43,22 +47,10 @@ namespace ThingsReportIt
 
             _AdminPW = config.GetValue<string>("AdminPW");
 
-
             try
             {
                 _BCclient = new BlobContainerClient(_ThingsStorageConnectionString, _ImagesContainer);
                 _BCclient.CreateIfNotExists(Azure.Storage.Blobs.Models.PublicAccessType.Blob);
-
-                //CloudStorageAccount storageAccount = CloudStorageAccount.Parse(vCS);
-                //CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
-                //imagesContainer = cloudBlobClient.GetContainerReference(strContainerName);
-                //imagesContainer.CreateIfNotExists();
-                //BlobContainerPermissions permissions = new BlobContainerPermissions
-                //{
-                //    PublicAccess = BlobContainerPublicAccessType.Blob
-                //};
-                //imagesContainer.SetPermissions(permissions);
-
             }
             catch (Exception e)
             {
